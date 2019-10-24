5faf170156b30f6050dd8d4ae74ebd47b2e82400
ufc2combate <erlitonfalucas@gmail.com>
2019-10-24 14:32:28 -0300

    Calculadora traduzida pra C#

---
+++ calculadora.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Escolha um número: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Escolha outro número: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("A soma entre esses números resulta em: " + (num1 + num2));
+            Console.WriteLine("A subtração entre esses números resulta em: " + (num1 - num2));
+            Console.WriteLine("A multiplicação entre esses números resulta em: " + (num1 * num2));
+            if (num2 != 0)
+            {
+                Console.WriteLine("A divisão entre esses números resulta em: " + (num1 / num2));
+            }
+            else
+            {
+                Console.WriteLine("A divisão entre esses números é impossível, já que o segundo número dado foi 0");
+            }
+        }
+    }
+}
